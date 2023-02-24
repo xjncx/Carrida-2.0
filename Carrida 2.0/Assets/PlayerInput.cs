b288001d8573ee440427e8dccbfe6cb6622c7754
xjncx <joncy4mm@gmail.com>
2023-02-24 20:33:08 +0300

    123

---
+++ Carrida 2.0/Assets/PlayerInput.cs
@@ -0,0 +1,176 @@
+// GENERATED AUTOMATICALLY FROM 'Assets/PlayerInput.inputactions'
+
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.InputSystem;
+using UnityEngine.InputSystem.Utilities;
+
+public class @PlayerInput : IInputActionCollection, IDisposable
+{
+    public InputActionAsset asset { get; }
+    public @PlayerInput()
+    {
+        asset = InputActionAsset.FromJson(@"{
+    ""name"": ""PlayerInput"",
+    ""maps"": [
+        {
+            ""name"": ""Player"",
+            ""id"": ""0419019c-72f6-4c2d-b6ce-1d37be377457"",
+            ""actions"": [
+                {
+                    ""name"": ""Move"",
+                    ""type"": ""Value"",
+                    ""id"": ""591e061c-1740-4a26-9322-4044f113315a"",
+                    ""expectedControlType"": ""Vector2"",
+                    ""processors"": """",
+                    ""interactions"": """"
+                }
+            ],
+            ""bindings"": [
+                {
+                    ""name"": ""2D Vector"",
+                    ""id"": ""9d9e28a1-3c1a-4681-8ca1-383c8e702540"",
+                    ""path"": ""2DVector"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""Move"",
+                    ""isComposite"": true,
+                    ""isPartOfComposite"": false
+                },
+                {
+                    ""name"": ""up"",
+                    ""id"": ""a72bf87c-fcfe-475e-ae9b-611e20a9afb6"",
+                    ""path"": ""<Keyboard>/w"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""Move"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": true
+                },
+                {
+                    ""name"": ""down"",
+                    ""id"": ""a4724346-22ef-4b3b-bf2a-9ab71b0f9ac7"",
+                    ""path"": ""<Keyboard>/s"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""Move"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": true
+                },
+                {
+                    ""name"": ""left"",
+                    ""id"": ""db1f335f-d6e5-43ba-9241-f859c36da42d"",
+                    ""path"": ""<Keyboard>/a"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""Move"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": true
+                },
+                {
+                    ""name"": ""right"",
+                    ""id"": ""d661c05c-5a63-4e2e-912b-602af778786d"",
+                    ""path"": ""<Keyboard>/d"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""Move"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": true
+                }
+            ]
+        }
+    ],
+    ""controlSchemes"": []
+}");
+        // Player
+        m_Player = asset.FindActionMap("Player", throwIfNotFound: true);
+        m_Player_Move = m_Player.FindAction("Move", throwIfNotFound: true);
+    }
+
+    public void Dispose()
+    {
+        UnityEngine.Object.Destroy(asset);
+    }
+
+    public InputBinding? bindingMask
+    {
+        get => asset.bindingMask;
+        set => asset.bindingMask = value;
+    }
+
+    public ReadOnlyArray<InputDevice>? devices
+    {
+        get => asset.devices;
+        set => asset.devices = value;
+    }
+
+    public ReadOnlyArray<InputControlScheme> controlSchemes => asset.controlSchemes;
+
+    public bool Contains(InputAction action)
+    {
+        return asset.Contains(action);
+    }
+
+    public IEnumerator<InputAction> GetEnumerator()
+    {
+        return asset.GetEnumerator();
+    }
+
+    IEnumerator IEnumerable.GetEnumerator()
+    {
+        return GetEnumerator();
+    }
+
+    public void Enable()
+    {
+        asset.Enable();
+    }
+
+    public void Disable()
+    {
+        asset.Disable();
+    }
+
+    // Player
+    private readonly InputActionMap m_Player;
+    private IPlayerActions m_PlayerActionsCallbackInterface;
+    private readonly InputAction m_Player_Move;
+    public struct PlayerActions
+    {
+        private @PlayerInput m_Wrapper;
+        public PlayerActions(@PlayerInput wrapper) { m_Wrapper = wrapper; }
+        public InputAction @Move => m_Wrapper.m_Player_Move;
+        public InputActionMap Get() { return m_Wrapper.m_Player; }
+        public void Enable() { Get().Enable(); }
+        public void Disable() { Get().Disable(); }
+        public bool enabled => Get().enabled;
+        public static implicit operator InputActionMap(PlayerActions set) { return set.Get(); }
+        public void SetCallbacks(IPlayerActions instance)
+        {
+            if (m_Wrapper.m_PlayerActionsCallbackInterface != null)
+            {
+                @Move.started -= m_Wrapper.m_PlayerActionsCallbackInterface.OnMove;
+                @Move.performed -= m_Wrapper.m_PlayerActionsCallbackInterface.OnMove;
+                @Move.canceled -= m_Wrapper.m_PlayerActionsCallbackInterface.OnMove;
+            }
+            m_Wrapper.m_PlayerActionsCallbackInterface = instance;
+            if (instance != null)
+            {
+                @Move.started += instance.OnMove;
+                @Move.performed += instance.OnMove;
+                @Move.canceled += instance.OnMove;
+            }
+        }
+    }
+    public PlayerActions @Player => new PlayerActions(this);
+    public interface IPlayerActions
+    {
+        void OnMove(InputAction.CallbackContext context);
+    }
+}
---
+++ Carrida 2.0/Assets/Scripts/CarController.cs
@@ -0,0 +1,88 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class CarController : MonoBehaviour
+{
+    [SerializeField] private Rigidbody _sphereRb;
+    [SerializeField] private Rigidbody _carRb;
+    [SerializeField] private float _forwardSpeed;
+    [SerializeField] private float _reverseSpeed;
+    [SerializeField] private float _turnSpeed;
+    [SerializeField] private LayerMask _ground;
+    [SerializeField] private float _alignToGroundTime;
+    [SerializeField] private float _modifiedDrag;
+
+    private PlayerInput _playerInput;
+    private Vector2 _direction;
+    private float _moveInput;
+    private float _turnInput;
+    private float _normalDrag;
+    private bool _isGrounded;
+
+    private void Awake()
+    {
+        _playerInput = new PlayerInput();
+    }
+
+    private void Start()
+    {
+        _sphereRb.transform.parent = null;
+        _carRb.transform.parent = null;
+        _normalDrag = _sphereRb.drag;
+    }
+
+    private void OnEnable()
+    {
+        _playerInput.Enable();
+    }
+
+    private void OnDisable()
+    {
+        _playerInput.Disable();
+    }
+
+    private void Update()
+    {
+        _direction = _playerInput.Player.Move.ReadValue<Vector2>();
+        _moveInput = _direction.y;
+        _turnInput = _direction.x;
+
+
+        Debug.Log("Direction X: " + _direction.x + "Direction Y: " + _direction.y);
+
+        float newRotation = _turnInput * _turnSpeed * Time.deltaTime * _moveInput;
+        if (_isGrounded)
+            transform.Rotate(0, newRotation, 0, Space.World);
+
+        transform.position = _sphereRb.transform.position;
+
+        RaycastHit hit;
+        _isGrounded = Physics.Raycast(transform.position, -transform.up, out hit, 1f, _ground);
+
+        Quaternion toRotateTo = Quaternion.FromToRotation(transform.up, hit.normal) * transform.rotation;
+        transform.rotation = Quaternion.Slerp(transform.rotation, toRotateTo, _alignToGroundTime * Time.deltaTime);
+
+        _moveInput *= _moveInput > 0 ? _forwardSpeed : _reverseSpeed;
+        Debug.Log("isGrounded: " + _isGrounded);
+        _sphereRb.drag = _isGrounded ? _normalDrag : _modifiedDrag;
+
+    }
+
+    private void FixedUpdate()
+    {
+        Debug.Log("isGrounded: " + _isGrounded);
+        if (_isGrounded)
+        {
+            _sphereRb.AddForce(transform.forward * _moveInput, ForceMode.Acceleration);
+        }
+        else
+        {
+            _sphereRb.AddForce(transform.up * -200f);
+        }
+        Debug.Log("isGrounded: " + _isGrounded);
+        _carRb.MoveRotation(transform.rotation);
+    }
+
+}
